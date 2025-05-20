15bde5764b7648bd0e4d7e5a501c6dbc45528f77
dhirajsuthar781 <drsuthar781@gmail.com>
2025-05-20 17:18:26 +0530

    bug:response of no route found

--- src/middlewares/default/notFound.js
+++ src/middlewares/default/notFound.js
@@ -1,4 +1,12 @@
+import { statusCode } from "../../utils/constants/statusCode.js";
+
 export default function notFound(req, res, next) {
-     res.status(404).json({ success: false, message: 'Route not found' });
-   }
-   
+ 
+  res.status(statusCode.NOT_FOUND).json({
+    success: false,
+    message: 'Route not found',
+    timestamp: new Date().toISOString(),
+    data: null,
+    route: req.originalUrl,
+  });
+}
